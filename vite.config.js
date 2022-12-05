b6b66aa5249218f35cffa9d67ee2a57be750be53
vdndev <developpement.vandernoot@gmail.com>
2022-12-05 13:49:48 +0100

    Chore(vite) Install Vite/Bootstrap environments

---
+++ vite.config.js
@@ -0,0 +1,15 @@
+import path from "path";
+
+export default {
+  root: path.resolve(__dirname, "src"),
+  resolve: {
+    alias: {
+      "~bootstrap": path.resolve(__dirname, "node_modules/bootstrap"),
+      "~bootstrap-icons": path.resolve(__dirname, "node_modules/bootstrap-icons"),
+    },
+  },
+  server: {
+    port: 5173,
+    hot: true,
+  },
+}
